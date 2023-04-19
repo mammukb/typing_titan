69d9e0320c7c231ae96d26abb993c8616934c4b7
AnanthuTD <ananthutd2021@gmail.com>
2023-04-19 12:10:44 +0530

    fixed time restarting on each new word in a series

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,186 +1,111 @@
-import { words } from "./words";
 import { useState, useEffect } from "react";
+import { words } from "./words";
 import "./Game.css";
 
 function Game() {
-    // to store a modified version of passage which is converted into jsx.
-    const [converted, setConverted] = useState([]);
-    const [wpm, setWpm] = useState(0);
+  // Remove isFirstKeyDown from local storage on refresh
+  window.onbeforeunload = function() {
+    localStorage.removeItem('isFirstKeyDown');
+  };
 
-    // to store current passage.
-    const [passage, setPassage] = useState("");
+  // Set up state variables
+  const [convertedPassage, setConvertedPassage] = useState([]);
+  const [wpm, setWpm] = useState(0);
+  const [passage, setPassage] = useState("");
+  const [totalTime, setTotalTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(90);
+  const [won, setWon] = useState(false);
+  const [accuracy, setAccuracy] = useState(100);
+  const [timer, setTimer] = useState(undefined);
+  const [wordCount, setWordCount] = useState(1);
+  const [usedWords, setUsedWords] = useState([]);
 
-    /* if passage = "abc g"
-            converted[0] = <div>
-                            <p>a</p>
-                            <p>b</p>
-                            <p>c</p>
-                        </div>
-            converted[1] = <p>&nbsp;</p>
-            converted[2] = <div><p>g</p></div>
-    */
-
-    const [totalTime, setTotalTime] = useState(0);
-    const [remainingTime, setremainingTime] = useState(90);
-    const [won, setwon] = useState(false);
-
-    // to check if the user has completed the current word.
-    function isWon(correctLetterCount, wordLength) {
-        if (correctLetterCount === wordLength) {
-            console.log(correctLetterCount, " : ", wordLength);
-            setwon(true);
-        }
+  // Check if the user has completed the current word
+  function isWordCompleted(correctLetterCount, wordLength) {
+    if (correctLetterCount === wordLength) {
+      setWon(true);
     }
+  }
 
-    const [accuracy, setaccuracy] = useState(100);
-    const [timer, settimer] = useState(undefined);
+  useEffect(() => {
+    let correctLetterCount = 0;
+    let time = 0;
+    let isFirstKeyDown = JSON.parse(localStorage.getItem('isFirstKeyDown')) ?? true;
 
-    useEffect(() => {
-        try {
-            for (let index = 0; index < passage.length; index++) {
-                console.log("index", index);
-                document.getElementById(`passage_${index}`).style.color =
-                    "white";
-            }
-        } catch (error) {}
-    }, [converted]);
+    // Convert passage string to JSX format
+    const passageArray = passage.split(" ");
+    const jsxArray = [];
+    let index = 0;
+    passageArray.forEach(word => {
+      const wordArray = word.split("");
+      const wordJSXArray = [];
+      wordArray.forEach(letter => {
+        const id = `passage_${index}`;
+        const jsx = <p key={id} id={id}>{letter}</p>;
+        wordJSXArray.push(jsx);
+        index++;
+      });
+      const wordJSX = <div key={`word_${index}`} className="passage">{wordJSXArray}</div>;
+      jsxArray.push(wordJSX);
+      const spaceJSX = <p key={`space_${index}`}>&nbsp;</p>;
+      jsxArray.push(spaceJSX);
+      index++;
+    });
+    setConvertedPassage(jsxArray);
 
-    useEffect(() => {
-        console.log("mounted");
+    // Set up event listener for key presses
+    let count = 0;
+    const keyDownHandler = (e) => {
+      if (e.key === "Backspace") {
+        count = count > 0 ? count - 1 : 0;
+        let pTag = document.getElementById(`passage_${count}`);
+        pTag.style.color = "white";
+        return;
+      }
 
-        let correctLetterCount = 0,
-            time = 0;
+      // Skip these keys
+      if (["Shift", "CapsLock", "Control"].includes(e.key) || count >= passage.length) {
+        return;
+      }
 
-        // converting passage into jsx (<div><p>single letter</p></div>) format.
+      // On first keyDown, start counting time and calculate WPM
+      if (isFirstKeyDown) {
+        localStorage.setItem('isFirstKeyDown', 'false');
+        setTimer(setInterval(() => {
+          time = time + 1;
+          setTotalTime(time);
+          setRemainingTime(90 - time);
 
-        let convertedPassage = []; // array to store modified passage.
+          const letterPerSec = correctLetterCount / time;
+          const secPerWord = 5 / letterPerSec;
+          const wordPerMin = parseInt(60 / secPerWord);
+          setWpm(wordPerMin);
+        }, 1000));
+      }
+      isFirstKeyDown = false;
 
-        let temp = []; // to store each letter of a word, which is in a p tag.
+      // Color change on each correct and wrong letter
+      const char = e.key;
+      if (passage[count] !== char) {
+        let pTag = document.getElementById(`passage_${count}`);
 
-        let wordIndex = 0; // to track where to insert next word.
-
-        // iterating through each letter of the passage
-
-        for (let index = 0; index < passage.length; index++) {
-            let letter = passage[index];
-
-            // if letter is not a space insert it make a p tag with it
-
-            if (letter !== " ") {
-                temp[index] = (
-                    <p key={`passage_${index}`} id={`passage_${index}`}>
-                        {letter}
-                    </p>
-                );
-            } else {
-                // if space, insert all the p tag to a div and store it in convertedPassage.
-
-                convertedPassage[wordIndex++] = (
-                    <div key={`word_${wordIndex}`} className="passage">
-                        {temp}
-                    </div>
-                );
-
-                // add space after each word
-
-                convertedPassage[wordIndex++] = (
-                    <p key={`passage_${index}`} id={`passage_${index}`}>
-                        &nbsp;
-                    </p>
-                );
-                document.getElementById(
-                    `passage_${index}`
-                ).style.backgroundColor = "transparent";
-
-                // clearing contents of temp.
-
-                temp = [];
-            }
-        }
-
-        // inserting last word into convertedPassage, since there is no space after the last word.
-
-        convertedPassage[wordIndex] = (
-            <div key={`word_${wordIndex}`} className="d-flex passage">
-                {temp}
-            </div>
-        );
-
-        setConverted(convertedPassage);
-
-        let firstKeyDown = true;
-        let count = 0;
-        const keyDownHandler = (e) => {
-            if (e.key === "Backspace") {
-                count = count > 0 ? count - 1 : 0;
-                let p_tag = document.getElementById(`passage_${count}`);
-                p_tag.style.color = "white";
-                return;
-            }
-
-            // skip these keys
-
-            if (
-                e.key === "Shift" ||
-                e.key === "CapsLock" ||
-                e.key === "Control" ||
-                count >= passage.length
-            ) {
-                return;
-            }
-
-            // on first keyDown start counting time in sec and calculate wpm(words per minute).
-
-            if (firstKeyDown) {
-                settimer(
-                    setInterval(
-                        /* this function will be called on each second */
-                        () => {
-                            time = time + 1;
-                            setTotalTime(time);
-                            setremainingTime(90 - time);
-
-                            // letters typed per second.
-
-                            let letterPerSec = correctLetterCount / time;
-
-                            /* The "word" is an average of 5 characters.
-                           To calculate WPM, simply take the number of words typed in a minute
-                           with no typos and divide by five. For example, if you type 100 words
-                           in a minute including spaces, your typing speed would be 20 WPM.
-                        */
-
-                            //    5 letters == a word
-
-                            let secPerWord = 5 / letterPerSec;
-                            let wordPerMin = parseInt(60 / secPerWord);
-
-                            setWpm(wordPerMin);
-                        },
-                        1000 // 1000 ms == 1 s.
-                    )
-                );
-            }
-            firstKeyDown = false;
-
+            
             // color change on each correct and wrong letter.
 
-            let char = e.key;
-            if (passage[count] !== char) {
-                let p_tag = document.getElementById(`passage_${count}`);
+           
                 if (passage[count] === " ") {
-                    p_tag.style.backgroundColor = "#f66";
+                    pTag.style.backgroundColor = "#f66";
                 } else {
-                    p_tag.style.color = "#f66";
+                    pTag.style.color = "#f66";
                 }
             } else {
                 let p_tag = document.getElementById(`passage_${count}`);
                 p_tag.style.color = "rgb(163, 238, 175)";
                 correctLetterCount++;
-                isWon(correctLetterCount, passage.length);
+                isWordCompleted(correctLetterCount, passage.length);
             }
             count++;
-            setaccuracy(
+            setAccuracy(
                 parseInt((correctLetterCount / 5 / (passage.length / 5)) * 100)
             );
         };
@@ -195,32 +120,43 @@ function Game() {
 
         return () => {
             document.removeEventListener("keydown", keyDownHandler);
-            setConverted(null);
+            setConvertedPassage(null);
             console.log("unmounted");
         };
     }, [passage]);
 
-    const [wordCount, setwordCount] = useState(1);
-    const [usedWords, setusedWords] = useState([]);
+    useEffect(() => {
+        try {
+            for (let index = 0; index < passage.length; index++) {
+                console.log("index", index);
+                document.getElementById(`passage_${index}`).style.color =
+                    "white";
+            }
+        } catch (error) {}
+    }, [convertedPassage]);
 
     function changePassage() {
         console.log("usedWords: ", usedWords);
 
         let index = null;
         do {
-            index = Math.floor(Math.random() * words.length);
+            index = (Math.floor(Math.random() * (words.length-1)));
         } while (index in usedWords);
         setPassage(words[index]);
-        setusedWords([...usedWords, index]);
-        setwordCount(wordCount + 1);
+        setUsedWords([...usedWords, index]);
+        setWordCount(wordCount + 1);
     }
 
     useEffect(() => {
         if (won) {
-            clearInterval(timer);
+            
             if (wordCount <= 10) {
                 changePassage();
-                setwon(false);
+                setWon(false);
+            }
+            else
+            {
+                clearInterval(timer);
             }
         }
     }, [won]);
@@ -247,7 +183,7 @@ function Game() {
             <div className="wordarea">
                 <div className="passage">
                     {/* passage */}
-                    {converted}
+                    {convertedPassage}
                 </div>
                 <div>
                     {/* WPM */}
